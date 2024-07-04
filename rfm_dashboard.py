43bec0e267035997e6d715ce120c18e7c0250615
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-07-04 16:07:45 +0530

    done

--- rfm_dashboard.py
+++ rfm_dashboard.py
@@ -120,7 +120,7 @@ st.markdown("""
 <div class='header'>
     <h1>RFM Analysis Dashboard</h1>
     <img src='https://img.icons8.com/fluency/48/000000/customer-insight.png'/>
-    <p>Analyze your customer segments based on Recency, Frequency, and Monetary values</p>
+    <p style="color:purple">Analyze your customer segments based on Recency, Frequency, and Monetary values</p>
 </div>
 """, unsafe_allow_html=True)
 
