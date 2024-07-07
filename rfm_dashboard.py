103b06f8051ebecf9166bf478b821427e4841993
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-07-07 22:36:58 +0530

    done

--- rfm_dashboard.py
+++ rfm_dashboard.py
@@ -150,7 +150,7 @@ st.markdown("""
         font-size: 1.5em;
         cursor: pointer;
         transition: background-color 0.3s ease;
-        margin-left: 170px;
+        margin-left: 140px;
     }
     .center-button {
         display: flex;
