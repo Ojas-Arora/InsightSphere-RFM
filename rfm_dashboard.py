a7bb58f2afcbcac3ed7e2790868583373e4e7d9e
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-07-07 17:40:39 +0530

    done

--- rfm_dashboard.py
+++ rfm_dashboard.py
@@ -139,8 +139,7 @@ st.markdown("""
         animation: fadeIn 3s ease-in;
     }
 
-    /* Dropdown text color change */
-    .streamlit-dropdown > div:first-child {
+    .selectbox > div:first-child {
         color: purple !important;
     }
 
