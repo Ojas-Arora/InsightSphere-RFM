e72c8db5b9b4b3798f8101dcd03680ab09342276
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-10-04 12:59:27 +0530

    done

--- rfm_dashboard.py
+++ rfm_dashboard.py
@@ -262,6 +262,10 @@ if analysis_type == "Comparison of RFM Segments":
         'font': {'size': 18, 'family': 'Arial', 'color': 'black'}  # Bold and larger size for legend title
     }
 )
+    fig_bar.update_yaxes(tickfont=dict(size=14, family='Arial', color='black', weight='bold'))
+
+     # Make the bar text bold (if necessary)
+    fig_bar.update_traces(textfont=dict(size=14, family='Arial', color='black', weight='bold'))
     st.plotly_chart(fig_bar)
 
     # Pie chart of percentage distribution
