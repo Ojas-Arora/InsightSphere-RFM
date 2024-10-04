4413d6c78b90b6ac19aa8a47337326bd3057a9ba
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-10-04 14:51:59 +0530

    done

--- rfm_dashboard.py
+++ rfm_dashboard.py
@@ -758,6 +758,23 @@ elif analysis_type == "Correlation Matrix of RFM Values within Champions Segment
 
     # Scatter plot of Recency vs Frequency
     fig_scatter_rf = px.scatter(champions_data, x='Recency', y='Frequency', title='Scatter Plot of Recency vs Frequency in Champions Segment', color='Monetary')
+    fig_scatter_rf.update_layout(
+    title={
+        'text': 'Scatter Plot of Recency vs Frequency in Champions Segment',
+        'font': {'size': 24, 'weight': 'bold', 'family': 'Arial'},  # Increase title font size and make it bold
+        'x': 0.5,  # Center the title
+        'xanchor': 'center',  # Align the title at the center
+        'yanchor': 'top'  # Keeps the title anchored at the top
+    },
+)
+
+    # Update font for x and y axis labels
+    fig_scatter_rf.update_xaxes(title_font=dict(size=18, weight='bold', family='Arial'))  # Bold and larger size for x-axis label
+    fig_scatter_rf.update_yaxes(title_font=dict(size=18, weight='bold', family='Arial'))  # Bold and larger size for y-axis label
+
+    # Update font for x and y tick labels
+    fig_scatter_rf.update_xaxes(tickfont=dict(size=16, family='Arial', weight='bold'))  # Increase and bold x-axis tick labels
+    fig_scatter_rf.update_yaxes(tickfont=dict(size=16, family='Arial', weight='bold'))  # Increase and bold y-axis tick labels
     st.plotly_chart(fig_scatter_rf)
 
     # Scatter plot of Monetary vs Frequency
